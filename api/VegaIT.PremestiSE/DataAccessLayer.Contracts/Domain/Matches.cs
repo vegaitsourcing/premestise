4b1b61dbf6c916354740a0517d38861999e77923
Dejan Varmedja <d.varmedja@vega.local>
2019-10-04 22:39:27 +0200

    ..

---
+++ api/VegaIT.PremestiSE/DataAccessLayer.Contracts/Domain/Matches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Contracts.Domain
+{
+    public enum Status
+    {
+        Approved,
+        Waiting
+    }
+    public class Matches
+    {
+        public int RequestOne { get; set; }
+        public int RequestTwo { get; set; }
+        public Status Status { get; set; }
+    }
+}
