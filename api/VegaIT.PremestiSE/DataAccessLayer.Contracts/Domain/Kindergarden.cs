570ee7de471670c8a1f66c96ab633986cb9ed03f
Dejan Varmedja <d.varmedja@vega.local>
2019-10-04 22:29:48 +0200

    Models

---
+++ api/VegaIT.PremestiSE/DataAccessLayer.Contracts/Domain/Kindergarden.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLayer.Contracts.Domain
+{
+    public class Kindergarden
+    {
+
+        public enum LocationType
+        {
+            Base,
+            Remote
+        }
+
+        public class Request
+        {
+            public int Id { get; set; }
+            public string Government { get; set; }
+            public string City { get; set; }
+            public string Name { get; set; }
+            public string Department { get; set; }
+            public string Street { get; set; }
+            public string Street_Number { get; set; }
+            public int PostalCode { get; set; }
+            public LocationType LocationType { get; set; }
+            public decimal Longitude { get; set; }
+            public decimal Latitude { get; set; }
+
+        }
+    }
+}
--- api/VegaIT.PremestiSE/DataAccessLayer.Contracts/Domain/Request.cs
+++ api/VegaIT.PremestiSE/DataAccessLayer.Contracts/Domain/Request.cs
@@ -4,8 +4,18 @@ using System.Text;
 
 namespace DataAccessLayer.Contracts.Domain
 {
+ 
+    
     public class Request
     {
+
         public int Id { get; set; }
+        public int ParentName { get; set; }
+        public string ChildName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public DateTime Date { get; set; }
+
     }
 }
