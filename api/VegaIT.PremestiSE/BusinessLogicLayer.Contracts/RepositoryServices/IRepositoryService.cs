e2f7ea1db0103a105b4385a9b4e18ff7d81d60e1
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-10-04 22:25:01 +0200

    added initial repository service contract

---
+++ api/VegaIT.PremestiSE/BusinessLogicLayer.Contracts/RepositoryServices/IRepositoryService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessLogicLayer.Contracts.RepositoryServices
+{
+    public interface IRepositoryService<TEntity, TPKey> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity Get(TPKey id);
+        TEntity Create(TEntity model);
+        TEntity Update(TEntity model);
+        void Delete(TPKey id);
+
+        //bool Validate();
+    }
+}
