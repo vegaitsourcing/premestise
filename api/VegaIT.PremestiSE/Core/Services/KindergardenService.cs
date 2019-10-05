1949c0cc80c75cf696c34bf1b0054cb44c61e58b
Dejan Varmedja <d.varmedja@vega.local>
2019-10-05 07:57:49 +0200

    DI

---
+++ api/VegaIT.PremestiSE/Core/Services/KindergardenService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Interfaces.Intefaces;
+using Core.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    public class KindergardenService : IKindergardenService
+    {
+        public IEnumerable<KindergardenDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ api/VegaIT.PremestiSE/Core/Services/RequestService.cs
@@ -0,0 +1,26 @@
+﻿using Core.Interfaces.Intefaces;
+using Core.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    public class RequestService : IRequestService
+    {
+        public RequestDto Create(RequestDto newRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RequestDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
