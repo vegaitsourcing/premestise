591fe057085bc09828cca7abb2b3d59937109b45
Nikola Mihalek <nikola.mihalek@gmail.com>
2019-10-06 16:18:51 +0200

    Add script for generating SQL database. Create SQL file

---
+++ api/db/util/sql_insert_generator.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import geo_point_fetcher
+import codecs
+
+class Kindergarden(object):
+    def __init__(self, *args, **kwargs):
+        asd = []
+        asd = list(args[0])
+        asd.extend([args[1]])
+        asd.extend([args[2]])
+        args = asd
+        self.uprava = args[0]
+        self.opstina = args[1]
+        self.naselje = args[2]
+        self.ustanova = args[3]
+        self.naziv = args[4]
+        self.ulica = str(args[5]).capitalize()
+        self.broj = args[6]
+        self.postanski_broj = args[7]
+        self.location_type = 1 if args[8] == 'Издвојена локација' else 0
+        self.latitude = str(args[9])
+        self.longitude = str(args[10])
+
+    def __str__(self):
+        return f'INSERT INTO kindergarden (city, municipality, government, department, name, street, street_number, postal_code, location_type) VALUES ({self.uprava}, {self.opstina}, {self.naselje}, {self.ustanova}, {self.naziv}, {self.ulica}, {self.broj}, {self.postanski_broj}, {self.location_type}, {self.latitude}, {self.longitude});'
+        
+obdanista = pd.read_csv('obdanista_csv_cyrillic.csv', delimiter = ',')
+dbRow = obdanista[['uprava','opstina','naselje','ustanova','naziv','ulica','broj','postanski_broj', 'tip_lokacije']]
+kindergardenAddress = obdanista[['ulica', 'broj', 'naselje']]
+with codecs.open('insert.sql', "a", "utf-8-sig") as f:
+    for i in range(1330, len(dbRow)-1):
+        adressLine = kindergardenAddress.iloc[i].to_string(header=False, index=False).strip()
+        point = geo_point_fetcher.getGeoPointFor(adressLine)
+        kindergarden = Kindergarden(dbRow.iloc[i], point.latitude if point is not None else "null", point.longitude if point is not None else "null")
+        print(i, kindergarden)
+        print(kindergarden, file=f)
