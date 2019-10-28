2450c1f1b3e8d5b9ae91e0829c189a0019ac996b
Mladen Milosevic <m.milosevic@vega.local>
2019-10-28 09:10:20 +0100

    added database translation script (cyr to lat)

--- api/db/util/sql_insert_generator.py
+++ api/db/util/sql_insert_generator.py
@@ -17,14 +17,15 @@ class Kindergarden(object):
         self.ulica = str(args[5]).capitalize()
         self.broj = args[6]
         self.postanski_broj = args[7]
-        self.location_type = 1 if args[8] == 'Издвојена локација' else 0
+        self.location_type = 1 if args[8] == 'Izdvojena lokacija' else 0
         self.latitude = str(args[9])
         self.longitude = str(args[10])
 
     def __str__(self):
         return f"INSERT INTO kindergarden (city, municipality, government, department, name, street, street_number, postal_code, location_type, longitude, latitude) VALUES ('{self.uprava}', '{self.opstina}', '{self.naselje}', '{self.ustanova}', '{self.naziv}', '{self.ulica}', '{self.broj}', '{self.postanski_broj}', {self.location_type}, {self.latitude}, {self.longitude});"
         
-obdanista = pd.read_csv('obdanista_csv_cyrillic.csv', delimiter = ',')
+# Ne sme error bad lines, gube se redovi, ali za sad radi ovo, mozda moze Header=1
+obdanista = pd.read_csv('obdanista_csv_latin.csv', delimiter = ',', error_bad_lines=False)
 dbRow = obdanista[['uprava','opstina','naselje','ustanova','naziv','ulica','broj','postanski_broj', 'tip_lokacije']]
 kindergardenAddress = obdanista[['ulica', 'broj', 'naselje']]
 with codecs.open('insert.sql', "a", "utf-8-sig") as f:
