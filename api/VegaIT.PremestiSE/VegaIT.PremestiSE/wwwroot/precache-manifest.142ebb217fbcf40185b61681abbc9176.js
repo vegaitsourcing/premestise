589033a084cf054a84eafe8f6a31cb5c88e8379c
babycatatonia <stefandvlp@gmail.com>
2020-03-23 14:46:16 +0100

    Implemented circular match

---
+++ api/VegaIT.PremestiSE/VegaIT.PremestiSE/wwwroot/precache-manifest.142ebb217fbcf40185b61681abbc9176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c63cc21517abd3379354a1dcd4d95e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e1b15ab532cd80e21c8",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "f784e3f3b6fe6dc1a906",
+    "url": "/static/js/2.cf6b5a08.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.cf6b5a08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e1b15ab532cd80e21c8",
+    "url": "/static/js/main.d50bde03.chunk.js"
+  },
+  {
+    "revision": "12dca864a85dabc6f146",
+    "url": "/static/js/runtime-main.a3f76e3b.js"
+  }
+]);
